56e25dcaf8d615882e12f51202cd43473fdaca83
marArcz <mar19arcz@gmail.com>
2023-04-26 11:37:08 +0800

    updated database file

--- electricity_billing_system (1).sql
+++ electricity_billing_system (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 24, 2023 at 03:50 AM
+-- Generation Time: Apr 26, 2023 at 05:36 AM
 -- Server version: 10.4.25-MariaDB
 -- PHP Version: 8.1.10
 
@@ -30,9 +30,11 @@ SET time_zone = "+00:00";
 CREATE TABLE `bills` (
   `id` int(11) NOT NULL,
   `connection_id` int(11) NOT NULL,
-  `month` int(11) NOT NULL,
-  `year` int(11) NOT NULL,
-  `created_at` datetime NOT NULL DEFAULT current_timestamp()
+  `rate` int(11) NOT NULL,
+  `units` int(11) NOT NULL,
+  `amount` int(11) NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT current_timestamp(),
+  `bill_no` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -45,9 +47,8 @@ CREATE TABLE `connections` (
   `id` int(11) NOT NULL,
   `customer_id` int(11) NOT NULL,
   `meter_number` varchar(255) NOT NULL,
-  `connection_type` varchar(255) NOT NULL,
   `connection_date` date NOT NULL,
-  `status` int(11) NOT NULL
+  `status` int(11) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -64,12 +65,18 @@ CREATE TABLE `customers` (
   `address` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+-- --------------------------------------------------------
+
 --
--- Dumping data for table `customers`
+-- Table structure for table `transactions`
 --
 
-INSERT INTO `customers` (`id`, `firstname`, `lastname`, `phone`, `address`) VALUES
-(1, 'John', 'Doe', '67654545', 'ljjkljkjkljkjklj');
+CREATE TABLE `transactions` (
+  `id` int(11) NOT NULL,
+  `bill_id` int(11) DEFAULT NULL,
+  `amount` int(11) NOT NULL,
+  `created_at` date NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -89,7 +96,7 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `fullname`, `username`, `password`) VALUES
-(1, 'admin', 'admin', 'admin');
+(1, 'Admin', 'admin', 'admin');
 
 --
 -- Indexes for dumped tables
@@ -99,19 +106,28 @@ INSERT INTO `users` (`id`, `fullname`, `username`, `password`) VALUES
 -- Indexes for table `bills`
 --
 ALTER TABLE `bills`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `bills_ibfk_1` (`connection_id`);
 
 --
 -- Indexes for table `connections`
 --
 ALTER TABLE `connections`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `customer_id` (`customer_id`);
 
 --
 -- Indexes for table `customers`
 --
 ALTER TABLE `customers`
   ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `transactions`
+--
+ALTER TABLE `transactions`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `transactions_ibfk_1` (`bill_id`);
 
 --
 -- Indexes for table `users`
@@ -139,13 +155,41 @@ ALTER TABLE `connections`
 -- AUTO_INCREMENT for table `customers`
 --
 ALTER TABLE `customers`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `transactions`
+--
+ALTER TABLE `transactions`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `bills`
+--
+ALTER TABLE `bills`
+  ADD CONSTRAINT `bills_ibfk_1` FOREIGN KEY (`connection_id`) REFERENCES `connections` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `connections`
+--
+ALTER TABLE `connections`
+  ADD CONSTRAINT `connections_ibfk_1` FOREIGN KEY (`customer_id`) REFERENCES `customers` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `transactions`
+--
+ALTER TABLE `transactions`
+  ADD CONSTRAINT `transactions_ibfk_1` FOREIGN KEY (`bill_id`) REFERENCES `bills` (`id`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
